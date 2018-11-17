1c48a3d70cb8375e82023f6a2b299529866e1a46
Jeorgius <V0tum-Separatum@yandex.ru>
2018-11-17 22:39:04 +0300

    discovery_server

---
+++ display_content/src/main/java/com/jeorgius/display_content/Controller/ConfigGet.java
@@ -0,0 +1,9 @@
+package com.jeorgius.display_content.Controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigGet {
+}
