45588782acf3599812a8e1478b0a86dcee2a77cb
Jeorgius <V0tum-Separatum@yandex.ru>
2018-11-18 22:28:24 +0300

    ConfigServer + DiscoveryServer + DisplayContent

--- cfg/src/main/java/com/jeorgius/cfg/CfgApplication.java
+++ cfg/src/main/java/com/jeorgius/cfg/CfgApplication.java
@@ -2,15 +2,13 @@ package com.jeorgius.cfg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.ResourceUtils;
 
 @SpringBootApplication
-@EnableConfigServer
-//@ComponentScan(basePackages = "com.jeorgius.cfg")
-//@PropertySource(ResourceUtils.CLASSPATH_URL_PREFIX + "database.properties")
+@ComponentScan(basePackages = "com.jeorgius.cfg")
+@PropertySource(ResourceUtils.CLASSPATH_URL_PREFIX + "database.properties")
 public class CfgApplication {
 
     public static void main(String[] args) {
