b9581573030ce6671150f4f65c6cefd87778aa63
Jeorgius <V0tum-Separatum@yandex.ru>
2018-12-08 22:55:17 +0300

    Built Angular index.html and set route to it

---
+++ front/src/main/java/com/jeorgius/front/rest/Rest.java
@@ -0,0 +1,11 @@
+package com.jeorgius.front.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Rest {
+  @RequestMapping("/")
+  public String mainPage(Model model){return "index";}
+}
