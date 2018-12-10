4f35decced0bd402a8c385cf9835c36a9bed0958
Jeorgius <V0tum-Separatum@yandex.ru>
2018-12-10 09:05:55 +0300

    Refactored Angular and handled 404 for front

--- front/src/main/java/com/jeorgius/front/cfg/Cfg.java
+++ front/src/main/java/com/jeorgius/front/cfg/Cfg.java
@@ -1,10 +1,6 @@
 package com.jeorgius.front.cfg;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc
--- front/src/main/java/com/jeorgius/front/rest/Rest.java
+++ front/src/main/java/com/jeorgius/front/rest/Rest.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Rest {
-  @RequestMapping("/")
-  public String mainPage(Model model){return "index";}
+  //handles 404 on refresh F5
+  @RequestMapping(value = "/**/{[path:[^\\.]*}")
+  public String mainPage(Model model){
+    return "forward:/";
+  }
 }
