8b17c2c4937b76019cb5675b77b473ff3842f753
Jeorgius <V0tum-Separatum@yandex.ru>
2019-03-16 22:00:43 +0300

    3D map works

--- front_ts/src/app/content/model3d/model3d.component.ts
+++ front_ts/src/app/content/model3d/model3d.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import * as mapboxgl from 'mapbox-gl';
-// import * as mgl from 'ngx-mapbox-gl';
+import * as mgl from 'ngx-mapbox-gl';
 // import {ControlComponent} from "ngx-mapbox-gl";
 
 @Component({
@@ -12,7 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Model3dComponent implements OnInit {
 
-  private adminToken = "sk.eyJ1IjoiamVvcmdpdXMiLCJhIjoiY2pzc3ZiZHcyMWNwMTN5bXo3anQ2Zmt4dCJ9.IwmNn-ya-SWGps7N37mdYw";
+  public centerXY =[
+
+    30.319943, 59.963848 // Piter
+  ];
+
+  public myWay = [[-122.48369693756104, 37.83381888486939],
+    [-122.48348236083984, 37.83317489144141],
+    [-122.48339653015138, 37.83270036637107],
+    [-122.48356819152832, 37.832056363179625],
+    [-122.48404026031496, 37.83114119107971],
+    [-122.48404026031496, 37.83049717427869],
+    [-122.48348236083984, 37.829920943955045],
+    [-122.48356819152832, 37.82954808664175],
+    [-122.48507022857666, 37.82944639795659],
+    [-122.48610019683838, 37.82880236636284],
+    [-122.48695850372314, 37.82931081282506],
+    [-122.48700141906738, 37.83080223556934],
+    [-122.48751640319824, 37.83168351665737],
+    [-122.48803138732912, 37.832158048267786],
+    [-122.48888969421387, 37.83297152392784],
+    [-122.48987674713133, 37.83263257682617],
+    [-122.49043464660643, 37.832937629287755],
+    [-122.49125003814696, 37.832429207817725],
+    [-122.49163627624512, 37.832564787218985],
+    [-122.49223709106445, 37.83337825839438],
+    [-122.49378204345702, 37.83368330777276]];
+
+  public data = {
+    'type': 'Feature',
+    'properties': {},
+    'layout':{
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    'paint': {
+      'line-color': '#FF0000',
+      'line-width': '8'
+    },
+    'geometry': {
+      'type': 'LineString',
+      'coordinates': [
+        [30.319943, 59.963848],
+        [30.319950, 59.963848],
+        [30.319970, 59.963848]
+      ]
+    }
+  };
+  // private adminToken = "sk.eyJ1IjoiamVvcmdpdXMiLCJhIjoiY2pzc3ZiZHcyMWNwMTN5bXo3anQ2Zmt4dCJ9.IwmNn-ya-SWGps7N37mdYw";
   constructor() { }
 
   ngOnInit() {
